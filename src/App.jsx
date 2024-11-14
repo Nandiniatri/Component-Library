95aab3cd2c1ed83e5bcfba9f4d2ce401e38b8365
unknown <ankitrajput10791@gmail.com.com>
2024-11-14 23:06:04 +0530

    something is missing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
 import './App.css'
-import DropDown from './Dropdown/dropdown' 
- 
 function App() {  
-  const options =["Drop-1","Drop-2","Drop-3","Drop-4","Drop-5"]
+
   return (
     <>
-     <h3>Feature Dropdown</h3>
-     <DropDown options={options} label={"Choose a Items"}/>
+
     </>
   )
 }
