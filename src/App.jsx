5171753ae4c70853ea53ef56974bd21bb9b5815c
Nandiniatri <atrinandini8@gmail.com>
2024-11-14 22:40:05 +0530

    updated file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
 import CartFile from './CartFile'
-import CartContextApi from './ContextApi/contextApi'
 
 function App() {
 
   return (
     <>
-      <CartFile />
     </>
   )
 }
